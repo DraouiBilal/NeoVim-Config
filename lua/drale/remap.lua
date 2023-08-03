20e4552b500e2e4baa0ce576eb3daf1795adfe68
Draoui Bilal <bilaldraoui22@gmail.com>
2023-08-03 20:03:21 +0200

    Changed remap to go to file explorer

--- lua/drale/remap.lua
+++ lua/drale/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "-", vim.cmd.Ex)
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
