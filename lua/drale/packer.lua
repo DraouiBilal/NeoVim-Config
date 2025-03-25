5be8a66549d71bceb8bc1e8594dc2bf19b9af96f
DraouiBilal <bilaldraoui22@gmail.com>
2025-03-25 06:33:25 +0100

    Fixed tokyonight

--- lua/drale/packer.lua
+++ lua/drale/packer.lua
@@ -8,10 +8,10 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-    use("folke/tokyonight.nvim")
+
+    use {"folke/tokyonight.nvim"}
 
     vim.cmd('colorscheme tokyonight-night')
 
