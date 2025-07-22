d4d8753652f5fee94c203b83696948e014ef775e
Bilal DRAOUI <bdraoui@ncelrnd2867.nce.amadeus.net>
2025-07-22 14:24:35 +0200

    Added git remaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.api.nvim_set_keymap('n', '<leader>gd', ':Git mergetool<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gb', ':Git blame<CR>', { noremap = true, silent = true })
